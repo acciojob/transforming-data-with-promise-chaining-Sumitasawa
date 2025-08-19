7fc1f7d7a64669c4abf3288367e7359ca8bf5d18
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-19 14:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+const input = document.getElementById("ip");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+  let num = Number(input.value);
+  output.textContent = "";
+  new Promise((resolve) => {
+    setTimeout(() => {
+      output.textContent = `Result: ${num}`;
+      resolve(num);
+    }, 2000);
+  })
+    .then((val) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          val = val * 2;
+          output.textContent = `Result: ${val}`;
+          resolve(val);
+        }, 1000);
+      });
+    })
+    
+    .then((val) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          val = val - 3;
+          output.textContent = `Result: ${val}`;
+          resolve(val);
+        }, 1000);
+      });
+    })
+    .then((val) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          val = val / 2;
+          output.textContent = `Result: ${val}`;
+          resolve(val);
+        }, 1000);
+      });
+    })
+    .then((val) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          val = val + 10;
+          output.textContent = `Final Result: ${val}`;
+          resolve(val);
+        }, 1000);
+      });
+    });
+});
